2b49bbe815d983949342c32d96b2bcd5b04d6fa9
leandro.chaves@nerdsoft.com <leandro.chaves@nerdsoft.com>
2024-10-17 23:01:59 -0300

    Initial commit

--- gravity-anti-spam.php
+++ gravity-anti-spam.php
@@ -19,8 +19,8 @@
  * Version:           1.0.0
  * Author:            Leandro Chaves
  * Author URI:        https://github.com/leochabu/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           MIT
+ * License URI:       https://opensource.org/license/MIT
  * Text Domain:       gravity-anti-spam
  * Domain Path:       /languages
  */
